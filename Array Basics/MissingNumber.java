e7afab5953dd99e7ff4fcf393578e687b2862804
guywithlightide <ksohams97@gmail.com>
2025-02-19 22:11:46 +0530

    max consecutive digits

--- Array Basics/MissingNumber.java
+++ Array Basics/MissingNumber.java
@@ -33,10 +33,40 @@ public class MissingNumber {
         }
         return -1;
     }
+
+
+    public static int bySummation(int [] arr, int N) {
+        int expected = (N*(N+1))/2;
+        int actual = 0;
+        for (int i = 0; i < arr.length; i++) {
+            actual+=arr[i];
+        }
+        return expected-actual;
+    }
+
+    public static int byXor(int [] arr, int N) {
+        
+        int xor1=0,xor2=0;
+
+        for (int i = 0; i < N - 2 ; i++) {
+            System.out.println(xor1 +"^"+(i+1)+" = "+(xor1^(i+1)));
+            System.out.println(xor2 +"^"+arr[i]+" = "+(xor2^arr[i]));
+
+            xor1 = xor1 ^ (i+1);
+            xor2 = xor2 ^ arr[i];    
+         }
+
+        System.out.println(xor1);
+        System.out.println(xor2);
+        return xor1 ^ xor2;
+
+    }
  
     public static void main(String[] args) {
         int [] array = {1,5,2,3}; // missing 4 
         System.out.println("Missing found - by brute force = "+findMissingBruteForce(array));
         System.out.println("Missing found - by hashing = "+hashing(array,5));
+        System.out.println("Missing found - by summation = "+bySummation(array, 5));
+        System.out.println("Missing found - by xor = "+byXor(array, 5));
     }
 }
